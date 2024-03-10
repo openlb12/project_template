8bea42385b92c6042e67c91e10cc95c524813057
Leaf <openlb12@163.com>
2024-03-10 23:32:18 +0800

    init python project tempalte

---
+++ lib/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@FileName  ：lib\__init__.py
+@Time      : 2024/03/10 23:11:58 
+@Author    : H.F. YE
+"""
+
+if __name__ == "__main__":
+
+    run_code = 0
